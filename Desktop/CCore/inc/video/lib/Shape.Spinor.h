dd0f5b547c9a072fb3df99750c7dbcd5bf25fa9e
Sergey Strukov <sshimnick@hotmail.com>
2017-02-16 23:08:10 +0300

    update

---
+++ Desktop/CCore/inc/video/lib/Shape.Spinor.h
@@ -0,0 +1,41 @@
+/* Shape.Spinor.h */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 3.00
+//
+//  Tag: Desktop
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2017 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#ifndef CCore_inc_video_lib_Shape_Spinor_h
+#define CCore_inc_video_lib_Shape_Spinor_h
+
+#include <CCore/inc/video/MinSizeType.h>
+#include <CCore/inc/video/Color.h>
+#include <CCore/inc/video/Font.h>
+#include <CCore/inc/video/RefVal.h>
+
+namespace CCore {
+namespace Video {
+
+/* classes */
+
+class SpinorShape;
+
+/* class SpinorShape */
+
+class SpinorShape
+ {
+ };
+
+} // namespace Video
+} // namespace CCore
+
+#endif
+
